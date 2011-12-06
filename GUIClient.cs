fd5182599e750bad76c5a326a9613d3ec93fd314
James Dunne <james.jdunne@gmail.com>
2011-12-05 21:54:04 -0600

    Populating DataGridView. Wow this control sucks.

--- GUIClient.cs
+++ GUIClient.cs
@@ -9,11 +9,14 @@ using System.Windows.Forms;
 using System.Net;
 using System.IO;
 using System.Diagnostics;
+using System.Collections;
+using System.Reflection;
 
 namespace rsatest
 {
     public partial class GUIClient : Form
     {
+        [STAThread]
         static void Main()
         {
             Application.Run(new GUIClient());
@@ -99,14 +102,94 @@ namespace rsatest
                     rsp = (HttpWebResponse)wex.Response;
                 }
 
+                // Read the response as JSON:
+                string json;
                 using (var rspstr = rsp.GetResponseStream())
                 using (var tr = new StreamReader(rspstr, Encoding.UTF8))
                 {
-                    string line;
-                    while ((line = tr.ReadLine()) != null)
-                        Debug.WriteLine(line);
+                    json = tr.ReadToEnd();
+                    Debug.WriteLine(json);
                 }
+
+                // Deserialize the JSON:
+                var jss = new System.Web.Script.Serialization.JavaScriptSerializer();
+                jss.RecursionLimit = 4;
+                jss.MaxJsonLength = int.MaxValue;
+                var dict = jss.Deserialize<Dictionary<string, object>>(json);
+                json = null;
+
+                object errObj;
+                if (dict.TryGetValue("error", out errObj))
+                {
+                    // Handle error responses:
+                    var errdict = (Dictionary<string, object>)errObj;
+                    foreach (KeyValuePair<string, object> pair in errdict)
+                        Debug.WriteLine(String.Format("error.{0} = {1}", pair.Key, pair.Value));
+                    errObj = null;
+                    return;
+                }
+
+                // Should have a successful result now:
+#if DEBUG
+                foreach (KeyValuePair<string, object> pair in dict)
+                    Debug.WriteLine(String.Format("rsp.{0} = {1}", pair.Key, pair.Value));
+#endif
+
+                ArrayList alHeader, alResults;
+                int timeMsec = (int)dict["time"];
+                alHeader = (ArrayList)dict["header"];
+                alResults = (ArrayList)dict["results"];
+
+                var header = (
+                    from Dictionary<string, object> h in alHeader
+                    select new { name = (string)h["name"], sqlTypeName = (string)h["type"], ordinal = (int)h["ordinal"] }
+                ).ToArray();
+
+                // Create a DataTable to represent the results:
+                DataTable dt = new DataTable();
+                var columns = (
+                    from h in header
+                    orderby h.ordinal ascending
+                    select new DataColumn(h.name, getCLRTypeForSQLType(h.sqlTypeName))
+                ).ToArray();
+                dt.Columns.AddRange(columns);
+                foreach (ArrayList row in alResults)
+                    dt.Rows.Add(row.ToArray());
+
+                UIMarshal(() =>
+                {
+                    var dg = dgQueryResults;
+
+                    // Set the grid's data source:
+                    doubleBuffered(dg, true);
+                    dg.AllowUserToAddRows = false;
+                    dg.AllowUserToDeleteRows = false;
+                    dg.AllowUserToOrderColumns = false;
+                    dg.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableWithAutoHeaderText;
+                    dg.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None;
+                    dg.AllowUserToResizeColumns = true;
+                    dg.AllowUserToResizeRows = true;
+                    dg.ReadOnly = true;
+                    dg.DataSource = dt;
+                    // Disable sorting:
+                    foreach (DataGridViewColumn column in dg.Columns)
+                    {
+                        column.SortMode = DataGridViewColumnSortMode.NotSortable;
+                    }
+                });
             }), null);
+        }
+
+        private static Type getCLRTypeForSQLType(string sqlTypeName)
+        {
+            return typeof(string);
+        }
+
+        private static void doubleBuffered(DataGridView dgv, bool setting)
+        {
+            Type dgvType = dgv.GetType();
+            PropertyInfo pi = dgvType.GetProperty("DoubleBuffered", BindingFlags.Instance | BindingFlags.NonPublic);
+            pi.SetValue(dgv, setting, null);
         }
     }
 }
