5d237e08793c95f4fa42b18e3374879ebb2c2a50
James Dunne <james.jdunne@gmail.com>
2012-06-19 00:15:13 -0500

    UI updates.

--- ChromeApp/main.js
+++ ChromeApp/main.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
     var theme = getTheme();
     $('#mainSplitter').jqxSplitter({ theme: theme, width: '100%', height: '100%', panels: [{ size: "20%", max: 800, min: 220 }, { size: "80%" }] });
     $('#querySplitter').jqxSplitter({ theme: theme, width: '100%', height: '100%', orientation: 'horizontal', panels: [{ size: "70%", collapsible: false }, { size: "30%", collapsible: true }] });
+
+    $('.cbxDatabase').jqxComboBox({ theme: theme, width: "100%", height: "100%", source: null, displayMember: "name", valueMember: "object_id", selectedIndex: 0 });
+
     var source = [
         { label: "Servers", expanded: true, items: [
             { label: "Server1", expanded: true, items: [
